de2e206df7cb7d799743a484f27736e96cef94da
mdihbinah <mdihbinah@gmail.com>
2026-05-18 18:36:53 +0600

    design Explore Cars for showing all car.

---
+++ src/app/explore-cars/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { cars } from '../../../public/carData';
+import CarCard from '@/component/home/CarCard';
+
+const ExploreCarsPages = () => {
+    return (
+        <div className='my-5'>
+            <h1 className='text-3xl text-center font-bold mb-5'>Explore Pages</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                {
+                    cars.map(car =>{
+                        return <CarCard key={car.id} car={car}></CarCard>
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default ExploreCarsPages;
